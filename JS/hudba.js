ce29957374e0202b9ebc7bdc5df9e14075d43025
Nia2003 <54575500+Nia2003@users.noreply.github.com>
2024-04-29 15:29:48 +0200

    Add files via upload

---
+++ JS/hudba.js
@@ -0,0 +1,232 @@
+const kpop = [
+  {
+    nazev: "Dreamcatcher",
+    popis: "Dreamcatcher je momentálně má nejoblíbenější k-pop rocková dívčí kapela. Jejich tvorbu převážně tvoří rocková hudba.",
+    video: "https://youtube.com/embed/AM7ro_nBCHI",
+},
+{
+  nazev: "BLACKPINK",
+  popis: "Kapela Blackpink je kapela, díky níž jsem začala poslouchat k-pop. Na tuto kapelu jsem přišla díky sledování K-pop Random Dance Game videí, ve kterých se tančilo na refrén písně Kill this love.",
+  video: "https://www.youtube.com/embed/QSaW4KB5IQk",
+},
+{
+  nazev: "EXO",
+  popis: "EXO je klučičí k-popová kapela, kterou jsem začala poslouchat jako první. Je to druhá takováto kapela, kterou jsem poznala.",
+  video: "https://www.youtube.com/embed/pSudEWBAYRE",
+},
+{
+  nazev: "EVERGLOW",
+  popis: "Dívčí skupinu Everglow moc neposlouchám, ale má podle mě jedny z nejhezčích choreografií.",
+  video: "https://www.youtube.com/embed/mi6ktNpIYGw",
+},
+{
+  nazev: "Day6",
+  popis: "Day6 je má nejoblíbenější k-pop rocková klučičí kapela. Jejich tvorbu tvoří hlavně rocková a punk-rocková hudba.",
+  video: "https://www.youtube.com/embed/g2X2LdJAIpU",
+},
+{
+  nazev: "ATEEZ",
+  popis: "Chlapecká kapela ATEEZ je má nejoblíbenější čistě k-popová kapela. Podle mě ATEEZ společně s kapelou SuperM mají nejlepší choreografie a videa v tomto žánru hudby.",
+  video: "https://www.youtube.com/embed/1gQ_-Jp5o7o",
+},]
+
+const metal = [
+  {
+    nazev: "Blackbriar",
+    popis: "Blackbriar je rock metalová gotická kapela. Začala jsem ji sledovat v roce 2014, kdy přidali na youtube první video.",
+    video: "https://www.youtube.com/embed/eP6xn8kZTeg",
+},
+{
+    nazev: "BABYMETAL",
+    popis: "Babymetal je japonská kawaii metalová skupina. Díky spojení J-popu a metalu je skupina originální a moc se mi díky tomu líbí.",
+    video: "https://www.youtube.com/embed/E8pcFhPZQYg",
+},
+{
+    nazev: "Diablo",
+    popis: "Diablo je metalová kapela, která po sedmi letech znovu začala produkovat hudbu. Jejich momentální tvorba je mnohem lepší než ta, kterou produkovali dříve. Díky posunu, který učinili, jsem velice ráda za jejich comeback.",
+    video: "https://www.youtube.com/embed/bMiJMy9kL_g?list=PLntCFKgv0lDdAExIPO-0nLf7NzqYM9vh7",
+},
+{
+    nazev: "Parkway Drive",
+    popis: "Parkway Drive je má nejoblíbenější kapela, která produkuje pouze metalcore. Tato kapela je mi velice simpatická tíky tomu, že vznikla v roce kdy jsem se narodila (2003).",
+    video: "https://www.youtube.com/embed/QmtRMoMWUKE",
+},
+{
+    nazev: "Infected Rain",
+    popis: "Infected Rain je matelová kapela. Kapela se mi líbí nejen kvůli skvělé zpěvačce ale i kvuli tomu, že produkují melodický death metal.",
+    video: "https://www.youtube.com/embed/2wA0skGcp5o",
+},
+{
+    nazev: "Slipknot",
+    popis: "Slipknot je metalová kapela. V čele kapeli je Corey Taylor, který je vinikajícím zpěvákem.",
+    video: "https://www.youtube.com/embed/4DkHdd9vdeE",
+},]
+
+const rock = [
+  {
+  nazev: "Rolling Quartz",
+  popis: "Rolling Quartz je dívčí korejská K-pop rocková kapela. Původně dělali jen rockové covery známých písní a až později začaly produkovat vlastní hudbu.",
+  video: "https://www.youtube.com/embed/Pg51wRHMRek",
+},
+{
+  nazev: "The Rasmus",
+  popis: "The Ramus je velice známá kapela, která se hraje i v rádiích. Pozornost této kapele jsem začala věnovat v páté třídě, kdy nám ji pan učitel na angličtinu pouštěl.",
+  video: "https://youtube.com/embed/7gwO8-oqwFw",
+},
+{
+  nazev: "Palaye Royale",
+  popis: "Palaye Royale je rocková kapela. Jejich tvorbu lze poznat ve filmu American Satan pod názvem kapely The Relentless.",
+  video: "https://youtube.com/embed/BJNLW7gHi5g",
+},
+{
+  nazev: "One OK Rock",
+  popis: "One OK Rock je japonská rocková kapela. Moc dlouho ji neposlouchám ale moc se mi líbí jak se text střídá v angličtině a japonštině.",
+  video: "https://youtube.com/embed/6YZlFdTIdzM?list=RDGMEMJQXQAmqrnmK1SEjY_rKBGA",
+},
+{
+  nazev: "The Pretty Reckless",
+  popis: "The Pretty Reckless je kapela, kterou poslouchám už velice dlouho. Zpěvačka této kapely má skvělý hlas, který se mi moc líbí.",
+  video: "https://youtube.com/embed/Hm4F_Eq_4tw?list=TLPQMjcwNTIwMjGsc4U73c-TKA",
+},
+{
+  nazev: "Bay Laurel",
+  popis: "Kapela Bay Laurel je vyloženě gotická kapela. Je to má nejoblíbenější gotická kapela.",
+  video: "https://youtube.com/embed/oCJDAN2vy9U",
+},]
+
+const klassic = [
+  {
+  nazev: "Johan Sebastian Bach",
+  popis: "Mezi mé neoblíbenější Bachovo skladby patří: Requiem, Fuga in a, d, h a g moll, Cello Suite No. 1, 2 a 6 a Choconne Partita No. 2...",
+  video: "https://www.youtube.com/embed/Nnuq9PXbywA",
+},
+{
+  nazev: "Ludwig van Bethoven",
+  popis: "Mezi mé oblíbené Bethovenovy skladby patří: Moonlight Sonata, Virus, Silence, 5th a 9th Symphony...",
+  video: "https://www.youtube.com/embed/mcufT_rncvM?list=TLPQMjcwNTIwMjGWb0gKzOVBdA",
+},
+{
+  nazev: "Charles Camille Saint-Saëns",
+  popis: "Saint-Saëns je můj nejoblíbenější skladatel a mezi mé nejoblíbenější jeho skladby patří: Danse Macabre, Aquarium, Cello Concerto No.1, La Cygne...",
+  video: "https://www.youtube.com/embed/YVpl-RNzdE4?list=TLPQMjcwNTIwMjGWb0gKzOVBdA",
+},
+{
+  nazev: "Fryderyk Franciszek Chopin",
+  popis: "Mezi mé nejoblíbenější Chopinovi skladby patří: Spring Waltz, Nocturno Op. 9 No. 2, 1 a 20...",
+  video: "https://www.youtube.com/embed/EFJ7kDva7JE?list=TLPQMjcwNTIwMjGWb0gKzOVBdA",
+},
+{
+  nazev: "Franz Liszt",
+  popis: "Mezi mé oblíbené jeho skladby patří: Love Dream, La Campanella, Hungarian Rhapsody, Un Saspiro, Sueño de amor...",
+  video: "https://www.youtube.com/embed/KpOtuoHL45Y?list=TLPQMjcwNTIwMjGWb0gKzOVBdA",
+},
+{
+  nazev: "Franz Peter Schubert",
+  popis: "Mezi mé nejoblíbenější Schubertovo skladby patří: Serenade, Ave Maria, Ständchen... a moje nejoblíbenějši je Erlkönig.",
+  video: "https://www.youtube.com/embed/UWNCbpwC-PQ?list=TLPQMjcwNTIwMjGWb0gKzOVBdA",
+},]
+
+const tlPredchoziK = document.querySelector('#tlPredchoziK');
+const tlNasledujiciK = document.querySelector('#tlNasledujiciK');
+const tlPredchoziM = document.querySelector('#tlPredchoziM');
+const tlNasledujiciM = document.querySelector('#tlNasledujiciM');
+const tlPredchoziR = document.querySelector('#tlPredchoziR');
+const tlNasledujiciR = document.querySelector('#tlNasledujiciR');
+const tlPredchoziS = document.querySelector('#tlPredchoziS');
+const tlNasledujiciS = document.querySelector('#tlNasledujiciS');
+
+const sectionKpop = document.querySelector('#section-kpop');
+const sectionMetal = document.querySelector('#section-metal');
+const sectionRock = document.querySelector('#section-rock');
+const sectionKlassic = document.querySelector('#section-klassic');
+
+let indexKpop = 0;
+let indexMetal = 0;
+let indexRock = 0;
+let indexKlassic = 0;
+
+vypis(indexKpop, kpop, sectionKpop);
+vypis(indexMetal, metal, sectionMetal);
+vypis(indexRock, rock, sectionRock);
+vypis(indexKlassic, klassic, sectionKlassic);
+
+tlPredchoziK.addEventListener('click', function(){
+    if(indexKpop === 0){
+      indexKpop = kpop.length - 1;
+    }else{
+      indexKpop--;
+    }
+    vypis(indexKpop, kpop, sectionKpop);
+  });
+
+  tlNasledujiciK.addEventListener('click', function(){
+    if(indexKpop === kpop.length - 1){
+      indexKpop = 0;
+    }else{
+      indexKpop++;
+    }
+    vypis(indexKpop, kpop, sectionKpop);
+  });
+
+  tlPredchoziM.addEventListener('click', function(){
+    if(indexMetal === 0){
+      indexMetal = metal.length - 1;
+    }else{
+      indexMetal--;
+    }
+    vypis(indexMetal, metal, sectionMetal);
+  });
+
+  tlNasledujiciM.addEventListener('click', function(){
+    if(indexMetal === metal.length - 1){
+      indexMetal = 0;
+    }else{
+      indexMetal++;
+    }
+    vypis(indexMetal, metal, sectionMetal);
+  });
+
+  tlPredchoziR.addEventListener('click', function(){
+    if(indexRock === 0){
+      indexRock = rock.length - 1;
+    }else{
+      indexRock--;
+    }
+    vypis(indexRock, rock, sectionRock);
+  });
+
+  tlNasledujiciR.addEventListener('click', function(){
+    if(indexRock === rock.length - 1){
+      indexRock = 0;
+    }else{
+      indexRock++;
+    }
+    vypis(indexRock, rock, sectionRock);
+  });
+
+  tlPredchoziS.addEventListener('click', function(){
+    if(indexKlassic === 0){
+      indexKlassic = klassic.length - 1;
+    }else{
+      indexKlassic--;
+    }
+    vypis(indexKlassic, klassic, sectionKlassic);
+  });
+
+  tlNasledujiciS.addEventListener('click', function(){
+    if(indexKlassic === klassic.length - 1){
+      indexKlassic = 0;
+    }else{
+      indexKlassic++;
+    }
+    vypis(indexKlassic, klassic, sectionKlassic);
+  });
+
+  function vypis(index, data, sekce){
+    const elNazev = sekce.querySelector('.nazev');
+    const elPopis = sekce.querySelector('.popis');
+    const elVideo = sekce.querySelector('.video');
+
+    elNazev.textContent = data[index].nazev;
+    elPopis.textContent = data[index].popis;
+    elVideo.setAttribute('src', data[index].video);}
